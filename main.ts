7072b602dc78ed60f77face71a55787b5b6e050b
hao0623 <131328836+hao0623@users.noreply.github.com>
2023-05-17 21:24:48 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showString("c")
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showString("d")
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showString("a")
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showString("b")
+})
